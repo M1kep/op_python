ba800dbc8b1647ecc6326b0c0722fe919a0c9f55
Michael Poutre <m1kep.my.mail@gmail.com>
2022-12-04 15:10:19 -0800

    fix: Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="op_python",
+    version="0.0.1",
+    author="Michael Poutre",
+    description="Small module for interacting with 1Password CLI to retrieve values",
+    url="https://github.com/m1kep/op_python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+)
